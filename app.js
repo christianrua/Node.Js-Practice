f4a0f1a164476083925095a8215ce98902437167
Christian Rua <christian.rua@gmail.com>
2020-05-01 10:42:27 -0500

    Firts commit

---
+++ app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+require('dotenv').config();
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app=express();
+
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true, useUnifiedTopology: true});
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+const userSchema= new mongoose.Schema ({
+    email:String,
+    password:String
+});
+
+
+userSchema.plugin(encrypt, {secret:process.env.SECRET,encryptedFields:['password']});
+
+
+
+const User=new mongoose.model("User",userSchema);
+
+app.route("/")
+.get(function(req,res){
+    res.render("home");
+});
+
+app.route("/login")
+.get(function(req,res){
+    res.render("login");
+})
+.post(function(req,res){
+    User.findOne({email:req.body.username},function(err,userFound){
+        if(err){
+            console.log(err);
+            res.send("<h1>invalidad email or password.</h1>");
+        } else {
+            if(userFound){
+                if(userFound.password==req.body.password){
+                    res.render("secrets");
+                } else {
+                    res.send("<h1>invalidad email or password.</h1>");
+                }
+            }
+        }
+    });
+});
+
+
+
+app.route("/register")
+.get(function(req,res){
+    res.render("register");
+})
+.post(function(req,res){
+    const user=new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    user.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+
+
+let port= process.env.PORT;
+if (port== null || port==""){
+  port=3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on sucessfully");
+});
